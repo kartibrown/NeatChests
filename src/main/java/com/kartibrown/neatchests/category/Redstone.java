30592e663b77a7a5d94050947eaaa6ffc54b3a24
Karti <tretris57@gmail.com>
2026-07-18 21:14:51 +0200

    Update Redstone.java

--- src/main/java/com/kartibrown/neatchests/category/Redstone.java
+++ src/main/java/com/kartibrown/neatchests/category/Redstone.java
@@ -3,35 +3,54 @@ package com.kartibrown.neatchests.category;
 import com.kartibrown.neatchests.Category;
 import org.bukkit.Material;
 
-import java.util.EnumSet;
-import java.util.Map;
-import java.util.Set;
-
 public final class Redstone extends Category {
+    private static final int REDSTONE = 0;
+
     public Redstone() {
         super(1);
 
-        final Map<Material, Integer> redstone = createMap(
+        setBaseWeight(REDSTONE, 1000);
+
+        final Material[] redstone = {
                 Material.REDSTONE,
                 Material.REDSTONE_BLOCK,
                 Material.REDSTONE_TORCH,
                 Material.REPEATER,
                 Material.COMPARATOR,
+                Material.DAYLIGHT_DETECTOR,
+                Material.LEVER,
+                Material.STONE_BUTTON,
+                Material.STONE_PRESSURE_PLATE,
+                Material.LIGHT_WEIGHTED_PRESSURE_PLATE,
+                Material.HEAVY_WEIGHTED_PRESSURE_PLATE,
+                Material.TRIPWIRE_HOOK,
                 Material.REDSTONE_LAMP,
                 Material.PISTON,
                 Material.STICKY_PISTON,
-                Material.HOPPER,
-                Material.REDSTONE_ORE
-        );
+                Material.DISPENSER,
+                Material.DROPPER,
+                Material.HOPPER
+        };
 
-        addMaterialIfExists(redstone, "OBSERVER", 100);
-        addMaterialIfExists(redstone, "COPPER_BULB");
+        for (final Material mat : redstone){
+            addWithAutoWeight(REDSTONE, mat);
+        }
 
-        items[0].putAll(redstone);
+        addMaterialIfExists(REDSTONE, "CRAFTER");
+
+        addMaterialIfExists(REDSTONE, "OBSERVER");
+        addMaterialIfExists(REDSTONE, "TARGET");
+        addMaterialIfExists(REDSTONE, "CALIBRATED_SCULK_SENSOR");
+        addMaterialIfExists(REDSTONE, "SCULK_SENSOR");
+        addMaterialIfExists(REDSTONE, "SCULK_SHRIEKER");
+        addMaterialIfExists(REDSTONE, "CALIBRATED_SCULK_SENSOR");
+        addMaterialIfExists(REDSTONE, "COPPER_BULB");
+
+        addWithAutoWeight(REDSTONE, Material.REDSTONE_ORE);
     }
 
     @Override
     public boolean tryAdd(final Material material) {
-        return items[0].containsKey(material);
+        return items[REDSTONE].containsKey(material);
     }
 }
