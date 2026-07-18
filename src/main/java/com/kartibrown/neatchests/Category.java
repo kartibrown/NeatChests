14b1a36bbbad2a7cc1cb382065ce96ff443c7fc7
Karti <tretris57@gmail.com>
2026-07-18 21:19:30 +0200

    Refactor Category weight management
    Rework Category to manage per-subcategory weights and auto-assignment. Expose MAX_WEIGHT, add nextAvailableWeights initialized to MAX_WEIGHT/2, and provide addWithAutoWeight, getWeightFor, and setBaseWeight helpers. Change addMaterialIfExists/addToCategory to accept a subCategoryIndex (and clamp weights). Remove legacy createMap/unneeded overloads and update imports. This centralizes weight logic, enforces clamping, and prevents auto-weights from dropping below Misc.MISC_MAX_WEIGHT.

--- src/main/java/com/kartibrown/neatchests/Category.java
+++ src/main/java/com/kartibrown/neatchests/Category.java
@@ -1,25 +1,29 @@
 package com.kartibrown.neatchests;
 
+import com.kartibrown.neatchests.category.Misc;
 import org.bukkit.Material;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.util.*;
 
 public abstract class Category {
-
-    protected static final int MAX_WEIGHT = 2000;
+    public static final int MAX_WEIGHT = 2000;
 
     protected final Map<Material, Integer>[] items;
+
+    private final int[] nextAvaiableWeights;
 
     @SuppressWarnings("unchecked")
     public Category(final int numberOfSubCategories) {
 
         items = (EnumMap<Material, Integer>[]) new EnumMap[numberOfSubCategories];
+        nextAvaiableWeights = new int[numberOfSubCategories];
 
         for (int i = 0; i < numberOfSubCategories; i++) {
             items[i] = new EnumMap<>(Material.class);
+
+            nextAvaiableWeights[i] = MAX_WEIGHT / 2;
         }
     }
 
@@ -31,20 +35,19 @@ public abstract class Category {
      */
     public abstract boolean tryAdd(final Material material);
 
-    public void add(final Material material){}
+    public void add(final Material material, final int weight) {
+    }
 
     /**
-     * Adds a material if it exists in this version of Minecraft
+     * Adds a material if it exists in this version of Minecraft with an automated weight
      *
-     * @param bukkitName Material Name
+     * @param subCategoryIndex which sub category to add it to
+     * @param bukkitName       Material Name
      */
-    protected final void addMaterialIfExists(final Map<Material, Integer> map,
-                                             final String bukkitName,
-                                             final int weight) {
+    protected final void addMaterialIfExists(final int subCategoryIndex, final String bukkitName) {
         try {
             final Material mat = Material.valueOf(bukkitName);
-            // Protects from going over MAX_WEIGHT and under 0
-            map.put(mat, Math.clamp(weight, 0, MAX_WEIGHT));
+            addWithAutoWeight(subCategoryIndex, mat);
         } catch (final IllegalArgumentException e) {
             // The block doesn't exist, but we catch
             // the exception quietly
@@ -52,66 +55,37 @@ public abstract class Category {
     }
 
     /**
-     * Adds a material if it exists in this version of Minecraft with a weight of (0)
+     * Add a material if it exists in this version of Minecraft with a fixed weight
      *
-     * @param bukkitName Material Name
+     * @param subCategoryIndex which sub category to add it to
+     * @param bukkitName       Material Name
+     * @param weight           The weight to be added
      */
-    protected final void addMaterialIfExists(final Map<Material, Integer> map,
-                                             final String bukkitName) {
-        addMaterialIfExists(map, bukkitName, 0);
-    }
-
-    /**
-     * Creates a weighted map from the given materials with a decreasing weight starting
-     * from half of MAX_WEIGHT.
-     *
-     * @param materials the materials to assign weights to, in order of priority
-     * @return a map containing the materials and their corresponding weights
-     */
-    @NotNull
-    protected final Map<Material, Integer> createMap(final Material @NonNull ... materials) {
-        return createMap(true, materials);
-    }
-
-    /**
-     * Creates a map from the given materials. The weights can either decrease
-     * sequentially to enforce strict sorting order, or be set to zero for standard sorting.
-     *
-     * @param weighted  if true, weights decrease sequentially starting from half of MAX_WEIGHT;
-     *                  if false, all materials receive a weight of 0.
-     * @param materials the materials to add to the map, in order of appearance.
-     * @return a high-performance EnumMap containing the materials and their assigned weights.
-     */
-    @NotNull
-    protected final Map<Material, Integer> createMap(final boolean weighted, final Material @NonNull ... materials) {
-        final Map<Material, Integer> map = new EnumMap<>(Material.class);
-
-        // If unweighted, we start at 0. If weighted, we start at MAX_WEIGHT / 2 to leave some headroom.
-        int currentWeight = (weighted) ? MAX_WEIGHT / 2 : 0;
-
-        for (final Material material : materials) {
-            map.put(material, currentWeight);
-            // Only decrease if we are actually using weights, and protect from going below 0
-            if (weighted) {
-                currentWeight = Math.max(currentWeight - 1, 0);
-            }
+    protected final void addMaterialIfExists(final int subCategoryIndex,
+                                             final String bukkitName,
+                                             final int weight) {
+        try {
+            final Material mat = Material.valueOf(bukkitName);
+            items[subCategoryIndex].put(mat, weight);
+        } catch (final IllegalArgumentException e) {
+            // The block doesn't exist, but we catch
+            // the exception quietly
         }
-
-        return map;
     }
 
     /**
      * Adds to the sub category map with a chosen weight
      */
     protected void addToCategory(final int subCategoryIndex, final Material material, final int weight) {
-        items[subCategoryIndex].put(material, weight);
+        items[subCategoryIndex].put(material, Math.clamp(weight, 0, MAX_WEIGHT));
     }
 
-    /**
-     * Adds to the sub category map with a weight of (0)
-     */
-    protected void addToCategory(final int subCategoryIndex, final Material material) {
-        addToCategory(subCategoryIndex, material, 0);
+    protected final void addWithAutoWeight(final int subCategoryIndex, final Material material) {
+        int currentWeight = nextAvaiableWeights[subCategoryIndex];
+        items[subCategoryIndex].put(material, currentWeight);
+
+        // protects it from going under misc's weight
+        nextAvaiableWeights[subCategoryIndex] = Math.max(currentWeight - 1, Misc.MISC_MAX_WEIGHT);
     }
 
     /*
@@ -122,8 +96,16 @@ public abstract class Category {
         return null;
     }
 
-    @Contract(pure = true)
-    public final Map<Material, Integer>[] getItems() {
-        return items;
+    public final @Nullable Integer getWeightFor(final @NotNull Material material) {
+        for (final Map<Material, Integer> subCategoryMap : items) {
+            if (subCategoryMap.containsKey(material)) {
+                return subCategoryMap.get(material);
+            }
+        }
+        return null;
+    }
+
+    protected final void setBaseWeight(final int subCategoryIndex, final int weight) {
+        nextAvaiableWeights[subCategoryIndex] = Math.clamp(weight, 0, MAX_WEIGHT);
     }
 }
