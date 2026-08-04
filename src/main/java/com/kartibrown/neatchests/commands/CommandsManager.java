1ff6a275d14c36da2f24c249bae43a7fc39c383e
Karti <tretris57@gmail.com>
2026-08-04 09:50:59 +0200

    Create CommandsManager.java

---
+++ src/main/java/com/kartibrown/neatchests/commands/CommandsManager.java
@@ -0,0 +1,9 @@
+package com.kartibrown.neatchests.commands;
+
+import io.papermc.paper.command.brigadier.Commands;
+
+public final class CommandsManager {
+    public CommandsManager() {
+        Commands.literal("neatchests");
+    }
+}
