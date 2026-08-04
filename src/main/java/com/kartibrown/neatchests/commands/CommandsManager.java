fea3d96a89ab2910c4640f5018392f75f025e9e6
Karti <tretris57@gmail.com>
2026-08-04 10:23:51 +0200

    add neatchests and reload commands
    no logic yet, but getting there

--- src/main/java/com/kartibrown/neatchests/commands/CommandsManager.java
+++ src/main/java/com/kartibrown/neatchests/commands/CommandsManager.java
@@ -4,6 +4,16 @@ import io.papermc.paper.command.brigadier.Commands;
 
 public final class CommandsManager {
     public CommandsManager() {
-        Commands.literal("neatchests");
+        Commands.literal("neatchests")
+                .executes(ctx -> {
+                    // Show help and version
+                    return 1;
+                })
+                .then(Commands.literal("reload")
+                        .executes(ctx -> {
+                            // Reload
+                            return 1;
+                        })
+                );
     }
 }
