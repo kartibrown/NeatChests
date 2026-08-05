e8e1aaf6f3576358b585703a0832e8762d90db19
Karti <tretris57@gmail.com>
2026-08-05 09:49:32 +0200

    registered and created commands
    added reload command logic and registered commands to plugin

--- src/main/java/com/kartibrown/neatchests/NeatChestsPlugin.java
+++ src/main/java/com/kartibrown/neatchests/NeatChestsPlugin.java
@@ -1,9 +1,13 @@
 package com.kartibrown.neatchests;
 
+import com.kartibrown.neatchests.commands.CommandsManager;
 import com.kartibrown.neatchests.config.ConfigManager;
 import com.kartibrown.neatchests.config.LoggerManager;
 import com.kartibrown.neatchests.listener.ChestStorageListener;
 import com.kartibrown.neatchests.sorting.SortingManager;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NeatChestsPlugin extends JavaPlugin {
@@ -11,19 +15,31 @@ public class NeatChestsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Config
-        final ConfigManager config = new ConfigManager(this);
-        config.load();
+        final ConfigManager configManager = new ConfigManager(this);
+        configManager.load();
 
         // Logger
-        final LoggerManager logger = new LoggerManager(this, config);
+        final LoggerManager loggerManager = new LoggerManager(this, configManager);
+
+        // Commands
+        final CommandsManager commandsManager = new CommandsManager();
+        final LiteralCommandNode<CommandSourceStack> buildCommand =
+                commandsManager.createCommandTree(configManager, loggerManager).build();
+
+        // Add commands to plugin
+        this.getLifecycleManager().registerEventHandler(
+                LifecycleEvents.COMMANDS,
+                commands -> {
+                    commands.registrar().register(buildCommand);
+                });
 
         // Sorting
-        final SortingManager sortingManager = new SortingManager(config, logger);
+        final SortingManager sortingManager = new SortingManager(configManager, loggerManager);
 
         getServer().getPluginManager().registerEvents(new ChestStorageListener(sortingManager)
                 , this);
 
-        logger.info("NeatChests has been enabled successfully!");
+        loggerManager.info("NeatChests has been enabled successfully!");
     }
 
     @Override
--- src/main/java/com/kartibrown/neatchests/commands/CommandsManager.java
+++ src/main/java/com/kartibrown/neatchests/commands/CommandsManager.java
@@ -1,19 +1,46 @@
 package com.kartibrown.neatchests.commands;
 
+import com.kartibrown.neatchests.config.ConfigManager;
+import com.kartibrown.neatchests.config.LoggerManager;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jspecify.annotations.NonNull;
 
 public final class CommandsManager {
     public CommandsManager() {
-        Commands.literal("neatchests")
-                .executes(ctx -> {
-                    // Show help and version
-                    return 1;
-                })
+
+    }
+
+    public LiteralArgumentBuilder<CommandSourceStack> createCommandTree(
+            final ConfigManager configManager,
+            final LoggerManager logger) {
+        return Commands.literal("neatchests")
+                .executes(this::help)
                 .then(Commands.literal("reload")
-                        .executes(ctx -> {
-                            // Reload
-                            return 1;
-                        })
+                        .executes(ctx -> reload(ctx, configManager, logger))
                 );
+    }
+
+    private int help(final CommandContext<CommandSourceStack> ctx) {
+        return 1;
+    }
+
+    private int reload(final @NonNull CommandContext<CommandSourceStack> ctx,
+                       final @NonNull ConfigManager configManager,
+                       final @NonNull LoggerManager logger) {
+        configManager.reload();
+        logger.info("Configuration reloaded via command.");
+
+        final CommandSender sender = ctx.getSource().getSender();
+        if (sender instanceof Player) {
+            sender.sendMessage("Config Reloaded!");
+        }
+
+        return 1;
     }
 }
