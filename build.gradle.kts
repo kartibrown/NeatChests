164a83e3a123052927d38119f11fa7734d6729ba
Karti <tretris57@gmail.com>
2026-08-11 07:58:31 +0200

    Exclude guava and gson from worldguard dependency
    Update build.gradle.kts to add excludes for com.google.guava:guava and com.google.code.gson:gson on the compileOnly com.sk89q.worldguard:worldguard-bukkit:7.0.18 dependency to prevent pulling transitive versions and avoid conflicts with the project's chosen libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,11 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.18")
+
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.18") {
+        exclude(group = "com.google.guava", module = "guava")
+        exclude(group = "com.google.code.gson", module = "gson")
+    }
 }
 
 java {
