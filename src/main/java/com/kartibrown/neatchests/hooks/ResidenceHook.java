8a1c6273347146e9dbe8cf4b771f51ab9c6ca54c
Karti <tretris57@gmail.com>
2026-08-12 08:56:06 +0200

    added bypass check for residence

--- src/main/java/com/kartibrown/neatchests/hooks/ResidenceHook.java
+++ src/main/java/com/kartibrown/neatchests/hooks/ResidenceHook.java
@@ -1,6 +1,8 @@
 package com.kartibrown.neatchests.hooks;
 
+import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
+import com.bekvon.bukkit.residence.containers.ResAdmin;
 import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -12,6 +14,10 @@ public final class ResidenceHook implements ProtectionHook {
 
     @Override
     public boolean canAccess(final Player player, final Location location) {
+        if (ResAdmin.isResAdmin(player)) {
+            return true;
+        }
+
         final FlagPermissions perms = FlagPermissions.getPerms(location);
 
         return perms.playerHas(player, Flags.container, true);
